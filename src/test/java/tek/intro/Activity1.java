25e99c0dbd6254c073ffed7f1811f4376b5d8fe0
mydocuments <hamed.mr.unique@gmail.com>
2024-07-09 18:27:28 -0700

    handle browser commit

---
+++ src/test/java/tek/intro/Activity1.java
@@ -0,0 +1,15 @@
+package tek.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+
+    public static void main(String[] args) {
+
+        ChromeDriver fbDriver = new ChromeDriver();
+        fbDriver.get("https://www.facebook.com/");
+
+        fbDriver.quit();
+
+    }
+}
---
+++ src/test/java/tek/intro/HandleBrowser.java
@@ -0,0 +1,14 @@
+package tek.intro;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleBrowser {
+    public static void main(String[] args) {
+        ChromeDriver name = new ChromeDriver();
+        name.get("https://www.google.com/");
+
+
+
+
+    }
+}
